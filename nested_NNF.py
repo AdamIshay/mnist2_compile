fe6f8f62cda2f4894385921faa51b2358aa57e02
AdamIshay <32616404+AdamIshay@users.noreply.github.com>
2023-05-22 17:32:35 -0700

    Update nested_NNF.py

--- nested_NNF.py
+++ nested_NNF.py
@@ -1,11 +1,16 @@
-from pysdd.sdd import SddManager, Vtree
+from pysdd.sdd import SddManager, Vtree, CompilerOptions
 
-vtree = Vtree(var_count=4, vtree_type="balanced")
+vtree = Vtree(var_count=2, vtree_type="balanced")
 sdd = SddManager.from_vtree(vtree)
-a, b, c, d = sdd.vars
+
+new_options = CompilerOptions(vtree_search_mode=0)
+sdd.set_options(new_options)
+#sdd.auto_gc_and_minimize_on()
+
+a, b = sdd.vars
 
 nnf = (a | ~b | (a & ~b)) & (b | ~a | (b & ~a)) # nested NNF (not DNF)
-dnf = (a & b) | (a & ~a) | (b & ~b) | (~a & ~b) | (a & b & ~a) | (a & b & ~b) | (a & ~a & ~b) | (b & ~a & ~b) | (a & b & ~a & ~b) # DNF
+dnf = (a & b) | (a & ~a) | (b & ~b) | (~a & ~b) | (a & b & ~a) | (a & b & ~b) | (a & ~a & ~b) | (b & ~a & ~b) | (a & b & ~a & ~b) # DNF, doesn't work with sdd.auto_gc_and_minimize_on()
 
 # equivalence checking
 print(nnf.equiv(dnf))
